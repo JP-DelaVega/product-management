ec0f111f6f80b2a92ed6efd0bea2ca2848b9ef4c
John Philip B Dela Vega <john.philip.dela.vega@ph.ey.com>
2025-07-01 07:55:39 +0800

    test

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.get("/api/health", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
   });
 }
 
